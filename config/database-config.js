dd5b468ab6891df531918e3446027f8784523b0e
Mohitgit22 <mohit.2022ug1007@iiitranchi.ac.in>
2024-01-16 22:02:59 +0530

    connected mongodb server

---
+++ config/database-config.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const connect = async () => {
+    await mongoose.connect("mongodb://localhost/chatapp");
+}
+
+module.exports = connect;
+
+
+
+
+
+
+
+
--- index.js
+++ index.js
@@ -6,17 +6,25 @@ const socketio = require('socket.io');
 const server = http.createServer(app);
 const io = socketio(server);
 
+
+const connect = require('./config/database-config');
+
+
+
 io.on('connection', (socket) => {
     console.log('a user connected', socket.id);
 
     socket.on('msg_send', (data) => {
         console.log(data);
         io.emit('msg_rcvd', data);
+        //socket.emit('msg_rcvd', data);
     });
 });
 
 app.use('/', express.static(__dirname + '/public'));
 
-server.listen(3000, () => {
+server.listen(3000, async () => {
     console.log('Server started');
+    await connect();
+    console.log("mongodb connected");
 });
